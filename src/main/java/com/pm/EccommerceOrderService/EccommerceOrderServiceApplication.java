5f48ac312b0798cbc43a071a85577a86c9917442
Shubham <shwillshine@gmail.com>
2025-07-19 16:30:43 +0530

    Registered order service on Eureka Server and fetched product service

--- src/main/java/com/pm/EccommerceOrderService/EccommerceOrderServiceApplication.java
+++ src/main/java/com/pm/EccommerceOrderService/EccommerceOrderServiceApplication.java
@@ -3,8 +3,10 @@ package com.pm.EccommerceOrderService;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EccommerceOrderServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pm/EccommerceOrderService/clients/ProductServiceClient.java
+++ src/main/java/com/pm/EccommerceOrderService/clients/ProductServiceClient.java
@@ -9,19 +9,20 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class ProductServiceClient {
 
-    private final RestTemplateBuilder restTemplateBuilder;
+    private final RestTemplate restTemplate;
 
-    public ProductServiceClient(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplateBuilder = restTemplateBuilder;
+
+    public ProductServiceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public ProductDTO getProductById(Long productId)
     {
-        RestTemplate template=restTemplateBuilder.build();
+      //  RestTemplate template=restTemplateBuilder.build();
 
-        String url="http://localhost:3001/api/products/" + productId;
+        String url="http://ECOMMERCEAPIINTEGRATION/api/products/" + productId;
 
-        ResponseEntity<ProductDTO> response= template.getForEntity(url, ProductDTO.class);
+        ResponseEntity<ProductDTO> response= restTemplate.getForEntity(url, ProductDTO.class);
         return response.getBody();
     }
 }
